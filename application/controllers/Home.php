4a021ea64e3bcab5667299300d278165ab5117ef
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-10-18 19:46:11 +0900

    Applied UI

---
+++ application/controllers/Home.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	private $login = false;
+
+	public function __construct() {
+		parent::__construct();
+
+		if ($this->session->admin) {
+			$this->login = true;
+		}
+	}
+
+
+	public function index() {
+		$this->load->view('header');
+		$this->load->view('sidebar');
+		$this->load->view('home/index');
+		$this->load->view('offset');
+		$this->load->view('scripts');
+	}
+}
