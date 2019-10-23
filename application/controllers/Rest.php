051d4ec809e60bbb8b70baa02233ed04290832f3
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-10-24 01:27:58 +0900

    Minor Update

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -13,10 +13,7 @@ class Rest extends REST_Controller {
 		if ($license) {
 			$this->response(['status' => TRUE], REST_Controller::HTTP_OK);
 		} else {
-			$this->response([
-				'status' => FALSE,
-				'message' => 'このマシンは登録されていません。'
-			], REST_Controller::HTTP_NOT_FOUND);
+			$this->response(['status' => FALSE, 'message' => 'このマシンは登録されていません。'], REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
 
@@ -30,10 +27,7 @@ class Rest extends REST_Controller {
 		$user = $this->Users_model->get_by_uid($username);
 		if ($user) {
 			if ($user['status'] == 0) {
-				$this->response([
-					'status' => FALSE,
-					'message' => 'このユーザーは無効になっています。'
-				], REST_Controller::HTTP_FORBIDDEN);
+				$this->response(['status' => FALSE, 'message' => 'このユーザーは無効になっています。'], REST_Controller::HTTP_FORBIDDEN);
 			} else {
 				if ($user['password'] == hash('sha256', $password)) {
 					if ($user['license']) {
@@ -42,44 +36,29 @@ class Rest extends REST_Controller {
 						$license = $this->Licenses_model->get_by_id($user['license']);
 
 						if ($license['expires'] && $this->expired($license['expires'])) {
-							$this->response([
-								'status' => FALSE,
-								'message' => 'ライセンスの期限が切れました。'
-							], REST_Controller::HTTP_FORBIDDEN);
+							$this->response(['status' => FALSE, 'message' => 'ライセンスの期限が切れました。'], REST_Controller::HTTP_FORBIDDEN);
 						} else {
 							if ($license['machine']) {
 								if ($license['machine'] == $machine) {
 									$this->set_response(['status' => TRUE], REST_Controller::HTTP_OK);
 								} else {
-									$this->response([
-										'status' => FALSE,
-										'message' => 'このライセンスはすでに使用中です。'
-									], REST_Controller::HTTP_FORBIDDEN);
+									$this->response(['status' => FALSE, 'message' => 'このライセンスはすでに使用中です。'], REST_Controller::HTTP_FORBIDDEN);
 								}
 							} else {
 								$this->Licenses_model->update_license($license['id'], array('machine' => $machine));
 
-								$this->set_response(['status' => TRUE], REST_Controller::HTTP_OK);
+								$this->set_response(['status' => TRUE, 'message' => 'OK'], REST_Controller::HTTP_OK);
 							}
 						}
 					} else {
-						$this->response([
-							'status' => FALSE,
-							'message' => 'このユーザーにはライセンスキーがありません。'
-						], REST_Controller::HTTP_FORBIDDEN);
+						$this->response(['status' => FALSE, 'message' => 'このユーザーにはライセンスキーがありません。'], REST_Controller::HTTP_FORBIDDEN);
 					}
 				} else {
-					$this->response([
-						'status' => FALSE,
-						'message' => '正しくないパスワード。'
-					], REST_Controller::HTTP_FORBIDDEN);
+					$this->response(['status' => FALSE, 'message' => '正しくないパスワード。'], REST_Controller::HTTP_FORBIDDEN);
 				}
 			}
 		} else {
-			$this->response([
-				'status' => FALSE,
-				'message' => '指定されたユーザーIDは存在しません。'
-			], REST_Controller::HTTP_NOT_FOUND);
+			$this->response(['status' => FALSE, 'message' => '指定されたユーザーIDは存在しません。'], REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
 
